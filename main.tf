080f730127196cc50cdd15ab38fe76359ba30302
Ayush Jain <ayushj0909+github@outlook.com>
2024-10-13 22:15:34 +0530

    chore(main): several configuration changes

--- main.tf
+++ main.tf
@@ -1,41 +1,58 @@
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
+locals {
+  # General
+  name            = var.cluster_name
+  region          = var.region
+  tags            = var.tags
+  cluster_version = var.kubernetes_version
+  # EKS
+  oidc_provider            = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+  iamproxy-service-account = "${var.cluster_name}-iamproxy-service-account"
+  # EKS Access
+  eks_access_iam_roles_map = { for role in var.eks_access_account_iam_roles : role.role_name => role }
+  eks_access_entries = merge(
+    { for role in data.aws_iam_roles.eks_access_iam_roles : role.name_regex => merge(local.eks_access_iam_roles_map[role.name_regex], { "arn" : tolist(role.arns)[0] }) },
+    { for role in var.eks_access_cross_account_iam_roles : role.role_name => merge({ "role_name" = role.role_name, "access_scope" = role.access_scope, "policy_name" = role.policy_name, "arn" = role.prefix != null ? format("arn:aws:iam::%s:role/%s/%s", role.account, role.prefix, role.role_name) : format("arn:aws:iam::%s:role/%s", role.account, role.role_name) }) }
+  )
+  # EKS Node Groups
+  default_critical_addon_nodegroup = {
+    instance_types = var.default_critical_addon_node_group_instance_types
+    ami_type       = "AL2023_ARM_64_STANDARD"
+    block_device_mappings = {
+      xvda = {
+        device_name = "/dev/xvda"
+        ebs = {
+          volume_size           = 100
+          volume_type           = "gp3"
+          encrypted             = true
+          delete_on_termination = true
+          kms_key_id            = var.default_critical_nodegroup_kms_key_id
+        }
+      }
+    }
+    min_size     = 3
+    max_size     = 3
+    desired_size = 3
+    labels = {
+      CriticalAddonsOnly = "true"
+    }
+    taints = {
+      addons = {
+        key    = "CriticalAddonsOnly"
+        value  = "true"
+        effect = "NO_SCHEDULE"
+      },
     }
   }
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
-  }
-}
-
-locals {
-  name   = "getting-started"
-  region = var.region
-
-  cluster_version = var.kubernetes_version
-
+  eks_managed_node_groups = merge(
+    { for k, v in var.eks_managed_node_groups : "${var.cluster_name}-${k}" => v },
+    var.create_default_critical_addon_node_group ? {
+      "truemark-system" = local.default_critical_addon_nodegroup
+    } : {}
+  )
+  # VPC
   vpc_cidr = var.vpc_cidr
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-
+  # GitOps URLs
   gitops_addons_url      = "${var.gitops_addons_org}/${var.gitops_addons_repo}"
   gitops_addons_basepath = var.gitops_addons_basepath
   gitops_addons_path     = var.gitops_addons_path
@@ -45,7 +62,7 @@ locals {
   gitops_workload_basepath = var.gitops_workload_basepath
   gitops_workload_path     = var.gitops_workload_path
   gitops_workload_revision = var.gitops_workload_revision
-
+  # GitOps Bridge AWS Addons
   aws_addons = {
     enable_cert_manager                          = try(var.addons.enable_cert_manager, false)
     enable_aws_efs_csi_driver                    = try(var.addons.enable_aws_efs_csi_driver, false)
@@ -73,6 +90,7 @@ locals {
     enable_ack_sfn                               = try(var.addons.enable_ack_sfn, false)
     enable_ack_eventbridge                       = try(var.addons.enable_ack_eventbridge, false)
   }
+  # GitOps Bridge OSS Addons
   oss_addons = {
     enable_argocd                          = try(var.addons.enable_argocd, true)
     enable_argo_rollouts                   = try(var.addons.enable_argo_rollouts, false)
@@ -96,14 +114,14 @@ locals {
     { kubernetes_version = local.cluster_version },
     { aws_cluster_name = module.eks.cluster_name }
   )
-
+  # GitOps Bridge Addons Metadata
   addons_metadata = merge(
     module.eks_blueprints_addons.gitops_metadata,
     {
       aws_cluster_name = module.eks.cluster_name
       aws_region       = local.region
       aws_account_id   = data.aws_caller_identity.current.account_id
-      aws_vpc_id       = module.vpc.vpc_id
+      aws_vpc_id       = var.create_vpc ? module.vpc.vpc_id : var.vpc_id
     },
     {
       addons_repo_url      = local.gitops_addons_url
@@ -118,11 +136,6 @@ locals {
       workload_repo_revision = local.gitops_workload_revision
     }
   )
-
-  tags = {
-    Blueprint  = local.name
-    GithubRepo = "github.com/gitops-bridge-dev/gitops-bridge"
-  }
 }
 
 ################################################################################
@@ -175,94 +188,123 @@ module "eks_blueprints_addons" {
 ################################################################################
 # EKS Cluster
 ################################################################################
-#tfsec:ignore:aws-eks-enable-control-plane-logging
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.13"
-
+  source                         = "terraform-aws-modules/eks/aws"
+  version                        = "~> 19.13"
   cluster_name                   = local.name
   cluster_version                = local.cluster_version
   cluster_endpoint_public_access = true
-
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    initial = {
-      instance_types = ["t3.medium"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-  }
+  vpc_id                         = var.create_vpc ? module.vpc.vpc_id : var.vpc_id
+  subnet_ids                     = var.create_vpc ? module.vpc.private_subnets : var.vpc_id
+  eks_managed_node_groups        = local.eks_managed_node_groups
   # EKS Addons
   cluster_addons = {
     coredns    = {}
     kube-proxy = {}
     vpc-cni = {
-      # Specify the VPC CNI addon should be deployed before compute to ensure
-      # the addon is configured before data plane compute resources are created
-      # See README for further details
-      before_compute = true
-      most_recent    = true # To ensure access to the latest settings provided
-      configuration_values = jsonencode({
-        env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-          ENABLE_PREFIX_DELEGATION = "true"
-          WARM_PREFIX_TARGET       = "1"
-        }
-      })
+      most_recent              = true
+      before_compute           = var.vpc_cni_before_compute
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+    }
+    eks-pod-identity-agent = {
+      most_recent = true
     }
     aws-ebs-csi-driver = {
+      most_recent              = true
       service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
     }
   }
   tags = local.tags
 }
+# IRSAs for EKS addons
 module "ebs_csi_driver_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.20"
-
-  role_name_prefix = "${module.eks.cluster_name}-ebs-csi-"
-
+  source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version               = "~> 5.20"
+  role_name_prefix      = "${module.eks.cluster_name}-ebs-csi-"
   attach_ebs_csi_policy = true
-
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
       namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
     }
   }
-
   tags = local.tags
 }
 
+module "vpc_cni_irsa" {
+  source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name             = "${var.cluster_name}-AmazonEKSVPCCNIRole"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+  tags = var.tags
+}
+# Storage Class
+resource "kubernetes_storage_class" "gp3_ext4_encrypted" {
+  metadata {
+    name = "gp3-ext4-encrypted"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Delete"
+  parameters = {
+    fsType    = "ext4"
+    type      = "gp3"
+    encrypted = "true"
+  }
+  volume_binding_mode = "WaitForFirstConsumer"
+  depends_on = [
+    aws_eks_access_entry.access_entries,
+    aws_eks_access_policy_association.access_policy_associations
+  ]
+}
+# EKS Access Configuration
+resource "aws_eks_access_entry" "access_entries" {
+  for_each      = local.eks_access_entries
+  cluster_name  = module.eks.cluster_name
+  principal_arn = each.value.arn
+  user_name     = "${each.key}:{{SessionName}}"
+}
+
+resource "aws_eks_access_policy_association" "access_policy_associations" {
+  for_each      = local.eks_access_entries
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/${each.value.policy_name}"
+  principal_arn = each.value.arn
+  dynamic "access_scope" {
+    for_each = each.value.access_scope != null ? [each.value.access_scope] : []
+    content {
+      type       = access_scope.value.type
+      namespaces = access_scope.value.namespaces != null ? access_scope.value.namespaces : []
+    }
+  }
+}
 ################################################################################
 # Supporting Resources
 ################################################################################
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-
+  count              = var.create_vpc ? 1 : 0
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "~> 5.0"
+  name               = local.name
+  cidr               = local.vpc_cidr
+  azs                = local.azs
+  private_subnets    = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
   enable_nat_gateway = true
   single_nat_gateway = true
-
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
   }
-
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
-
   tags = local.tags
 }
