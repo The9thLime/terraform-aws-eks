1753e39fc591476640e8f50655f66b57d36b6af6
Ayush Jain <ayushj0909+github@outlook.com>
2024-10-14 14:45:58 +0530

    chore(main): removed unrequired addons; added istio metadata

--- main.tf
+++ main.tf
@@ -72,50 +72,26 @@ locals {
 
   # GitOps Bridge AWS Addons
   aws_addons = {
-    enable_cert_manager                          = try(var.addons.enable_cert_manager, false)
-    enable_aws_efs_csi_driver                    = try(var.addons.enable_aws_efs_csi_driver, false)
-    enable_aws_fsx_csi_driver                    = try(var.addons.enable_aws_fsx_csi_driver, false)
-    enable_aws_cloudwatch_metrics                = try(var.addons.enable_aws_cloudwatch_metrics, false)
-    enable_aws_privateca_issuer                  = try(var.addons.enable_aws_privateca_issuer, false)
-    enable_cluster_autoscaler                    = try(var.addons.enable_cluster_autoscaler, false)
-    enable_external_dns                          = try(var.addons.enable_external_dns, false)
-    enable_external_secrets                      = try(var.addons.enable_external_secrets, false)
-    enable_aws_load_balancer_controller          = try(var.addons.enable_aws_load_balancer_controller, false)
-    enable_fargate_fluentbit                     = try(var.addons.enable_fargate_fluentbit, false)
-    enable_aws_for_fluentbit                     = try(var.addons.enable_aws_for_fluentbit, false)
-    enable_aws_node_termination_handler          = try(var.addons.enable_aws_node_termination_handler, false)
-    enable_karpenter                             = try(var.addons.enable_karpenter, false)
-    enable_velero                                = try(var.addons.enable_velero, false)
-    enable_aws_gateway_api_controller            = try(var.addons.enable_aws_gateway_api_controller, false)
-    enable_aws_ebs_csi_resources                 = try(var.addons.enable_aws_ebs_csi_resources, false)
-    enable_aws_secrets_store_csi_driver_provider = try(var.addons.enable_aws_secrets_store_csi_driver_provider, false)
-    enable_ack_apigatewayv2                      = try(var.addons.enable_ack_apigatewayv2, false)
-    enable_ack_dynamodb                          = try(var.addons.enable_ack_dynamodb, false)
-    enable_ack_s3                                = try(var.addons.enable_ack_s3, false)
-    enable_ack_rds                               = try(var.addons.enable_ack_rds, false)
-    enable_ack_prometheusservice                 = try(var.addons.enable_ack_prometheusservice, false)
-    enable_ack_emrcontainers                     = try(var.addons.enable_ack_emrcontainers, false)
-    enable_ack_sfn                               = try(var.addons.enable_ack_sfn, false)
-    enable_ack_eventbridge                       = try(var.addons.enable_ack_eventbridge, false)
+    enable_cert_manager                 = try(var.addons.enable_cert_manager, false)
+    enable_aws_cloudwatch_metrics       = try(var.addons.enable_aws_cloudwatch_metrics, false)
+    enable_cluster_autoscaler           = try(var.addons.enable_cluster_autoscaler, false)
+    enable_external_dns                 = try(var.addons.enable_external_dns, false)
+    enable_external_secrets             = try(var.addons.enable_external_secrets, false)
+    enable_aws_load_balancer_controller = try(var.addons.enable_aws_load_balancer_controller, false)
+    enable_karpenter                    = try(var.addons.enable_karpenter, false)
+    enable_aws_ebs_csi_resources        = try(var.addons.enable_aws_ebs_csi_resources, false)
   }
 
   # GitOps Bridge OSS Addons
   oss_addons = {
-    enable_argocd                          = try(var.addons.enable_argocd, true)
-    enable_argo_rollouts                   = try(var.addons.enable_argo_rollouts, false)
-    enable_argo_events                     = try(var.addons.enable_argo_events, false)
-    enable_argo_workflows                  = try(var.addons.enable_argo_workflows, false)
-    enable_cluster_proportional_autoscaler = try(var.addons.enable_cluster_proportional_autoscaler, false)
-    enable_gatekeeper                      = try(var.addons.enable_gatekeeper, false)
-    enable_gpu_operator                    = try(var.addons.enable_gpu_operator, false)
-    enable_ingress_nginx                   = try(var.addons.enable_ingress_nginx, false)
-    enable_keda                            = try(var.addons.enable_keda, false)
-    enable_kyverno                         = try(var.addons.enable_kyverno, false)
-    enable_kube_prometheus_stack           = try(var.addons.enable_kube_prometheus_stack, false)
-    enable_metrics_server                  = try(var.addons.enable_metrics_server, false)
-    enable_prometheus_adapter              = try(var.addons.enable_prometheus_adapter, false)
-    enable_secrets_store_csi_driver        = try(var.addons.enable_secrets_store_csi_driver, false)
-    enable_vpa                             = try(var.addons.enable_vpa, false)
+    enable_argocd                 = try(var.addons.enable_argocd, true)
+    enable_istio_base             = try(var.addons.enable_istio_base, false)
+    enable_istio_discovery        = try(var.addons.enable_istio_discovery, false)
+    enable_istio_internal_gateway = try(var.addons.enable_istio_internal_gateway, false)
+    enable_istio_external_gateway = try(var.addons.enable_istio_internal_gateway, false)
+    enable_kube_prometheus_stack  = try(var.addons.enable_kube_prometheus_stack, false)
+    enable_metrics_server         = try(var.addons.enable_metrics_server, false)
+    enable_vpa                    = try(var.addons.enable_vpa, false)
   }
 
   addons = merge(
@@ -134,6 +110,10 @@ locals {
       aws_account_id   = data.aws_caller_identity.current.account_id
       aws_vpc_id       = var.create_vpc ? module.vpc.vpc_id : var.vpc_id
     },
+    {
+      istio_public_subnet_ids = data.aws_subnets.istio_private_subnet_ids
+      istio_private_subnets_ids = data.aws_subnets.istio_private_subnet_ids
+    }
     {
       addons_repo_url      = local.gitops_addons_url
       addons_repo_basepath = local.gitops_addons_basepath
@@ -194,20 +174,12 @@ module "eks_blueprints_addons" {
 
   # EKS Blueprints Addons
   enable_cert_manager                 = local.aws_addons.enable_cert_manager
-  enable_aws_efs_csi_driver           = local.aws_addons.enable_aws_efs_csi_driver
-  enable_aws_fsx_csi_driver           = local.aws_addons.enable_aws_fsx_csi_driver
   enable_aws_cloudwatch_metrics       = local.aws_addons.enable_aws_cloudwatch_metrics
-  enable_aws_privateca_issuer         = local.aws_addons.enable_aws_privateca_issuer
   enable_cluster_autoscaler           = local.aws_addons.enable_cluster_autoscaler
   enable_external_dns                 = local.aws_addons.enable_external_dns
   enable_external_secrets             = local.aws_addons.enable_external_secrets
   enable_aws_load_balancer_controller = local.aws_addons.enable_aws_load_balancer_controller
-  enable_fargate_fluentbit            = local.aws_addons.enable_fargate_fluentbit
-  enable_aws_for_fluentbit            = local.aws_addons.enable_aws_for_fluentbit
-  enable_aws_node_termination_handler = local.aws_addons.enable_aws_node_termination_handler
   enable_karpenter                    = local.aws_addons.enable_karpenter
-  enable_velero                       = local.aws_addons.enable_velero
-  enable_aws_gateway_api_controller   = local.aws_addons.enable_aws_gateway_api_controller
 
   tags = local.tags
 }
